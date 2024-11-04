80a7a2feb750e7094b208072ffe2a547496dfdda
Joel Bodenmann <jbo@insane.engineer>
2024-11-04 19:19:30 +0100

    wip

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 
 #include "hal/clint/clint.hpp"
+#include "hal/plic/plic.hpp"
 #include "hal/uart/uart.hpp"
 #include "utils/bytes.hpp"
 
@@ -16,6 +17,7 @@ trap()
 
 #define UART_A 0x10001000
 #define CLINT  0x10010000
+#define PLIC   0x10C00000
 
 [[noreturn]]
 int
@@ -25,6 +27,7 @@ main()
 
     hal::uart uart(UART_A);
     hal::clint clint(CLINT);
+    hal::plic plic(PLIC);
 
     clint.setup(100'000'000);
 
