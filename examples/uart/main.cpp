08c849279ceb5149149cc434f3054ab14cf24083
Joel Bodenmann <jbo@insane.engineer>
2024-11-22 20:26:47 +0100

    examples/uart: Introduce example

--- examples/uart/main.cpp
+++ examples/uart/main.cpp
@@ -1,5 +1,9 @@
 #include <hal/clint/clint.hpp>
+#include <hal/plic/plic.hpp>
+#include <hal/uart/uart.hpp>
+#include <utils/bytes.hpp>
 
+#define UART_A 0x10001000
 #define CLINT  0x10010000
 
 extern "C"
@@ -15,9 +19,12 @@ main()
 {
     using namespace std::chrono_literals;
 
+    hal::uart uart(UART_A);
     hal::clint clint(CLINT);
 
     clint.setup(100'000'000);
+
+    uart.write(utils::to_bytes("Hello Vexii!\n"));
 
     while (true) {
         clint.delay(10ms);
