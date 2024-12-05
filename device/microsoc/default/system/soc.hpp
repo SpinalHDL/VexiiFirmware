9fc46d054dab2de29ec60c4baa58797bc6d8c167
Dolu1990 <charles.papon.90@gmail.com>
2024-12-05 10:56:36 +0100

    Move drivers around

--- device/microsoc/default/system/soc.hpp
+++ device/microsoc/default/system/soc.hpp
@@ -2,18 +2,18 @@
 
 #include "peripherals.hpp"
 
-#include <hal/clint/clint.hpp>
-#include <hal/plic/plic.hpp>
-#include <hal/uart/uart.hpp>
+#include <hal/riscv/clint/clint.hpp>
+#include <hal/riscv/plic/plic.hpp>
+#include <hal/spinal/uart/uart.hpp>
 
 #include <chrono>
 
 namespace soc
 {
 
-    static hal::clint clint(CLINT);
-    static hal::plic plic(PLIC);
-    static hal::uart uart(UART_A);
+    static hal::riscv::clint clint(CLINT);
+    static hal::riscv::plic plic(PLIC);
+    static hal::spinal::uart uart(UART_A);
 
     /**
      * Initialize the SoC.
