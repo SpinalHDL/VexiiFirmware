7f4e221233f65269da398cddbc9a863287f5a11b
Dolu1990 <charles.papon.90@gmail.com>
2024-12-05 11:37:27 +0100

    add target_setup_asm

--- app/setup.cmake
+++ app/setup.cmake
@@ -1,6 +1,6 @@
 include(${PROJECT_SOURCE_DIR}/cmake/device/vexiiriscv.cmake)
 
-function(target_setup_default TARGET)
+function(target_setup_cpp TARGET)
 
     target_setup_riscv(${TARGET})
 
@@ -23,6 +23,31 @@ function(target_setup_default TARGET)
         PRIVATE
             hal
             system
+    )
+
+endfunction()
+
+function(target_setup_asm TARGET)
+
+    target_setup_riscv(${TARGET})
+
+    target_compile_features(
+        ${TARGET}
+            PRIVATE
+            cxx_std_23
+    )
+
+    target_link_options(
+        ${TARGET}
+        PRIVATE
+            #--specs=nosys.specs
+            #"LINKER:--gc-sections"
+            #"LINKER:-flto"
+    )
+
+    target_link_libraries(
+        ${TARGET}
+        PRIVATE
     )
 
 endfunction()
